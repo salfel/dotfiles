f3918d67762701fb993b8b250f7c546243a46e82
Felix Salcher <dev.felix.salcher@gmail.com>
2024-03-02 14:22:43 +0100

    Automatically install Oh My Zsh and it's plugins

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/zsh
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
