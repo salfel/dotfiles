d202f3c82bd6a73c8fc018562c9c4e59b77756d5
Felix Salcher <dev.felix.salcher@gmail.com>
2024-03-03 11:02:26 +0100

    changed lsp_document_symbols to lsp_workspace_symbols to search through entire project

--- .config/nvim/lua/salfel/plugins/telescope.lua
+++ .config/nvim/lua/salfel/plugins/telescope.lua
@@ -34,6 +34,6 @@ return {
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 		vim.keymap.set('n', '<leader>fh', builtin.oldfiles, {})
-        vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+        vim.keymap.set('n', '<leader>fs', builtin.lsp_workspace_symbols, {})
 	end
 }
