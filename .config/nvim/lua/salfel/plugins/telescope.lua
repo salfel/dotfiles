907bc4648b98c9868fff996ab6df2d6304b26301
Felix Salcher <dev.felix.salcher@gmail.com>
2024-03-03 11:05:39 +0100

    remove workspace symbols as they are not working

--- .config/nvim/lua/salfel/plugins/telescope.lua
+++ .config/nvim/lua/salfel/plugins/telescope.lua
@@ -34,6 +34,5 @@ return {
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 		vim.keymap.set('n', '<leader>fh', builtin.oldfiles, {})
-        vim.keymap.set('n', '<leader>fs', builtin.lsp_workspace_symbols, {})
 	end
 }
