ca2679fc0063bd91d29637d8065e7e7e9fc53139
Felix Salcher <dev.felix.salcher@gmail.com>
2024-05-07 22:36:51 +0200

    extended checks for root files for formatters

--- nvim/lua/salfel/plugins/none-ls.lua
+++ nvim/lua/salfel/plugins/none-ls.lua
@@ -4,10 +4,27 @@ return {
         local null_ls = require("null-ls")
         local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+        local checkRoot = function(utils, files)
+            for _, file in ipairs(files) do
+                if utils.root_has_file(file) then
+                    return true
+                end
+            end
+            return false
+        end
+
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.gofmt,
-                null_ls.builtins.formatting.goimports,
+                null_ls.builtins.formatting.gofmt.with({
+                    condition = function(utils)
+                        return checkRoot(utils, { "go.mod" })
+                    end
+                }),
+                null_ls.builtins.formatting.goimports.with({
+                    condition = function(utils)
+                        return checkRoot(utils, { "go.mod" })
+                    end
+                }),
                 null_ls.builtins.formatting.biome.with({
                     condition = function(utils)
                         return utils.root_has_file("biome.json")
@@ -15,14 +32,17 @@ return {
                 }),
                 null_ls.builtins.formatting.pint.with({
                     condition = function(utils)
-                        return utils.root_has_file("vendor/bin/pint")
+                        return checkRoot(utils, { ".pint.json", "vendor/bin/pint" })
                     end
                 }),
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettier.with({
                     extra_filetypes = { "astro" },
                     condition = function(utils)
-                        return utils.root_has_file(".prettierrc")
+                        return checkRoot(utils,
+                            { ".prettierrc", ".prettierrc.json", ".prettierrc.yaml", ".prettierrc.yml", ".prettierrc.js",
+                                ".prettierrc.cjs", ".prettierrc.mjs", "prettier.config.js", "prettier.config.cjs",
+                                "prettier.config.mjs", "prettier.config.ts" })
                     end
                 })
             },
