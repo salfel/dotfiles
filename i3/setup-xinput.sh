adbe09fa94ce036d428613ee4a2fd741cca552a0
Felix Salcher <dev.felix.salcher@gmail.com>
2024-06-09 17:00:57 +0200

    removed i3 and all correlated dependencies due do no longer using i3

--- i3/setup-xinput.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-touchpad_id=$(xinput list | grep "Touchpad" | awk -F'=' '{print $2}' | awk '{print $1}')
-
-xinput set-prop "$touchpad_id" "libinput Tapping Enabled" 1
-xinput set-prop "$touchpad_id" 295 1
