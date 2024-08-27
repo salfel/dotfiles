fd6f4eaf69f080d90a4ce79d617294a8f5f8520a
Felix Salcher <dev.felix.salcher@gmail.com>
2024-08-27 22:37:58 +0200

    fixed script

--- hypr/scripts/disable-screen.sh
+++ hypr/scripts/disable-screen.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-default_monitor='eDP-1'
-monitors=$(xrandr | grep connected | awk '{print $1}')
+default_monitor=eDP-1
+monitors=$(xrandr | grep connected | awk '{print $1}' | wc -l)
 
-found=0
-
-for monitor in $monitors; do
-    if [ $monitor = $default_monitor ]; then 
-        found=1
-    fi
-done
-
-if [ $found -eq 1 ]; then 
+if [ $monitors -eq 2 ]; then 
     hyprctl keyword monitor $default_monitor,disable
 else 
     hyprctl keyword monitor $default_monitor,preferred,auto,1
