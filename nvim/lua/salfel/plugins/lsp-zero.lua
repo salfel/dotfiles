163866634ad0c1e0e1a01a507a7604e9ac21204f
Felix Salcher <felix.salcher@additive.eu>
2024-08-30 16:26:37 +0200

    added display of icons based on token in nvim-cmp

--- nvim/lua/salfel/plugins/lsp-zero.lua
+++ nvim/lua/salfel/plugins/lsp-zero.lua
@@ -8,6 +8,7 @@ return {
         "L3MON4D3/LuaSnip",
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+        "onsails/lspkind.nvim"
     },
     config = function()
         local lsp_zero = require("lsp-zero")
@@ -91,7 +92,24 @@ return {
             },
             mapping = cmp.mapping.preset.insert({
                 ["<CR>"] = cmp.mapping.confirm({ select = true })
-            })
+            }),
+            window = {
+                completion = cmp.config.window.bordered()
+            },
+            formatting = {
+                format = function(entry, vim_item)
+                    if vim.tbl_contains({ 'path' }, entry.source.name) then
+                        local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
+                        if icon then
+                            vim_item.kind = icon
+                            vim_item.kind_hl_group = hl_group
+                            return vim_item
+                        end
+                    end
+
+                    return require('lspkind').cmp_format({ with_text = true })(entry, vim_item)
+                end
+            }
         })
     end
 }
