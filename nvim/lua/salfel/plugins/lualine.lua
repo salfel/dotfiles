c8b5958b76f03c2423dc80dc780d184429db4136
Felix Salcher <felix.salcher@additive.eu>
2024-08-30 15:41:04 +0200

    display relative filepath

--- nvim/lua/salfel/plugins/lualine.lua
+++ nvim/lua/salfel/plugins/lualine.lua
@@ -10,7 +10,7 @@ return {
                 lualine_a = { "mode" },
                 lualine_b = { "branch", "diff" },
                 lualine_c = { "diagnostics" },
-                lualine_x = { "filename" },
+                lualine_x = { { "filename", path = 1 } },
                 lualine_y = { "filetype" }
             }
         })
