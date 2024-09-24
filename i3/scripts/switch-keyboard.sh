ac6fd4265cfdc5d4faca7dc690d5bdfd7bd80885
Felix Salcher <dev.felix.salcher@gmail.com>
2024-09-24 22:08:10 +0200

    Revert "removed i3, polybar, rofi, spicetify and openproject"
    This reverts commit 8bd920504dc4e5a12bbe0dc26cbfae030533d739.

---
+++ i3/scripts/switch-keyboard.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+current_layout=$(setxkbmap -query | grep layout | awk '{print $2}')
+if [ "$current_layout" = "us" ]; then
+  setxkbmap de
+else
+  setxkbmap us
+fi
---
+++ spicetify/Extensions/fullAppDisplay.js
@@ -0,0 +1,1 @@
+fullAppDisplay.js
