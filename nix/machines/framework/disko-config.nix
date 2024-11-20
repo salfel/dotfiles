61402fab2dc2de64af92ecf3df21f391f84e2487
Felix Salcher <dev.felix.salcher@gmail.com>
2024-11-20 14:06:37 +0100

    use absolute path

--- nix/machines/framework/disko-config.nix
+++ nix/machines/framework/disko-config.nix
@@ -1,15 +1,9 @@
-# USAGE in your configuration.nix.
-# Update devices to match your hardware.
-# {
-#  imports = [ ./disko-config.nix ];
-#  disko.devices.disk.main.device = "/dev/sda";
-# }
 {
   disko.devices = {
     disk = {
       main = {
         type = "disk";
-        device = "nvme0n1";
+        device = "/dev/nvme0n1";
         content = {
           type = "gpt";
           partitions = {
