1b9bb5e1209f2d9170557e06b85774f02e3016c0
Felix Salcher <dev.felix.salcher@gmail.com>
2024-12-05 21:19:23 +0100

    updated kitty config

--- nix/home/shell/kitty.nix
+++ nix/home/shell/kitty.nix
@@ -11,7 +11,8 @@
       confirm_os_window_close = 0;
       enable_audio_bell = false;
       window_padding_width = 12;
-      background_opacity = 0.95;
+      background_opacity = 0.9;
+      background_blur = 32;
     };
 
     extraConfig = ''
