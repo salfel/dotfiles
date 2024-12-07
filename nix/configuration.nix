0629c6b5eba71cb68d05f2ce64e7ac1d642a7639
Felix Salcher <dev.felix.salcher@gmail.com>
2024-12-07 21:37:50 +0100

    update kernel to 6.11 hardened

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -9,7 +9,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_6_12;
+  boot.kernelPackages = pkgs.linuxPackages_6_11_hardened;
 
   networking.hostName = "nixos"; # Define your hostname.
 
