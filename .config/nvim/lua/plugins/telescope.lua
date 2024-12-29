f8c0caf38447e511c17e48b4de716f9d897a59cf
Felix Salcher <dev.felix.salcher@gmail.com>
2024-12-29 22:21:18 +0100

    make live_grep search also inside dotfiles

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,6 +10,16 @@ return {
 
 		require("telescope").setup({
 			defaults = {
+                vimgrep_arguments = {
+                      'rg',
+                      '--color=never',
+                      '--no-heading',
+                      '--with-filename',
+                      '--line-number',
+                      '--column',
+                      '--smart-case',
+                      '--hidden',
+                    },
 				file_ignore_patterns = { "node_modules", "vendor", ".git/", "target", "dist", ".pio" },
 				mappings = {
 					i = {
