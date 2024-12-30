2e9b84fda2a6c8cf4cc87c394a663dadf25c5949
Felix Salcher <dev.felix.salcher@gmail.com>
2024-12-30 20:38:29 +0100

    move cmp to own file

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,43 @@
+return {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "L3MON4D3/LuaSnip",
+        "onsails/lspkind.nvim",
+    },
+    config = function()
+        local cmp = require("cmp")
+
+        cmp.setup({
+            sources = {
+                { name = "path" },
+                { name = "nvim_lsp" },
+                { name = "nvim_lua" },
+                { name = "buffer" },
+                { name = "luasnip" },
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            }),
+            window = {
+                completion = cmp.config.window.bordered(),
+            },
+            formatting = {
+                format = function(entry, vim_item)
+                    if vim.tbl_contains({ "path" }, entry.source.name) then
+                        local icon, hl_group = require("nvim-web-devicons").get_icon(entry.completion_item.label)
+                        if icon then
+                            vim_item.kind = icon
+                            vim_item.kind_hl_group = hl_group
+                            return vim_item
+                        end
+                    end
+
+                    return require("lspkind").cmp_format({ with_text = true })(entry, vim_item)
+                end,
+                expandable_indicator = false,
+                fields = { "abbr", "kind", "menu" },
+            },
+        })
+    end
+}
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,12 +1,8 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "onsails/lspkind.nvim",
     },
     config = function()
         require("mason").setup({})
@@ -44,39 +40,5 @@ return {
         vim.keymap.set("n", "gl", vim.diagnostic.open_float)
         vim.keymap.set("n", "K", vim.lsp.buf.hover)
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-
-        local cmp = require("cmp")
-
-        cmp.setup({
-            sources = {
-                { name = "path" },
-                { name = "nvim_lsp" },
-                { name = "nvim_lua" },
-                { name = "buffer" },
-                { name = "luasnip" },
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
-            }),
-            window = {
-                completion = cmp.config.window.bordered(),
-            },
-            formatting = {
-                format = function(entry, vim_item)
-                    if vim.tbl_contains({ "path" }, entry.source.name) then
-                        local icon, hl_group = require("nvim-web-devicons").get_icon(entry.completion_item.label)
-                        if icon then
-                            vim_item.kind = icon
-                            vim_item.kind_hl_group = hl_group
-                            return vim_item
-                        end
-                    end
-
-                    return require("lspkind").cmp_format({ with_text = true })(entry, vim_item)
-                end,
-                expandable_indicator = false,
-                fields = { "abbr", "kind", "menu" },
-            },
-        })
     end,
 }
