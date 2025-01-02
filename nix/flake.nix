61c4a5e2a6ef81e089cdf0874d3f43ecf9d07cf9
Felix Salcher <dev.felix.salcher@gmail.com>
2025-01-02 14:59:51 +0100

    catppuccin nixpkgs follows nixpkgs

--- nix/flake.nix
+++ nix/flake.nix
@@ -8,6 +8,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     catppuccin.url = "github:catppuccin/nix";
+    catppuccin.inputs.nixpkgs.follows = "nixpkgs";
 
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
     zen-browser.inputs.nixpkgs.follows = "nixpkgs";
