e3b232ba37e11bbe7da8dbe82ab2df3e3d15dfaf
Felix Salcher <dev.felix.salcher@gmail.com>
2025-01-04 22:51:58 +0100

    change option for pulseaudio from hardware to service

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -50,8 +50,8 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  # sound
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
