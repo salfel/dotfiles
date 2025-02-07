5b4d2e89f63a84fa84ebf5cc7c1a0cdc0d6b4342
Felix Salcher <dev.felix.salcher@gmail.com>
2025-02-07 10:11:16 +0100

    update pickers

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,55 +1,70 @@
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    ---@type snacks.Config
-    opts = {
-        --- @type snacks.explorer.Config
-        explorer = {
-            replace_netrw = true,
-        },
-        notifier = {
-            enabled = true,
-        },
-        picker = {
-            sources = {},
-            win = {
-                list = {
-                    keys = {
-                        ["<Esc>"] = { "close", mode = { "n", "i" } },
-                    },
-                },
-            },
-        },
-    },
+	"folke/snacks.nvim",
+	priority = 1000,
+	---@type snacks.Config
+	opts = {
+		--- @type snacks.explorer.Config
+		explorer = {
+			replace_netrw = true,
+		},
+		notifier = {
+			enabled = true,
+		},
+		picker = {
+			sources = {},
+			win = {
+				list = {
+					keys = {
+						["<Esc>"] = { "close", mode = { "n", "i" } },
+					},
+				},
+			},
+		},
+	},
 
-    config = function()
-        local snacks = require("snacks")
+	config = function()
+		local snacks = require("snacks")
 
-        snacks.indent.enable()
+		local window = {
+			input = {
+				keys = {
+					["<Esc>"] = { "close", mode = { "n", "i" } },
+				},
+			},
+		}
+		local finder = {
+			hidden = true,
+			win = window,
+		}
+		local config = {
+			files = finder,
+			explorer = finder,
+			grep = finder,
+			recent = finder,
+			lsp_workspace_symbols = { win = window },
+		}
 
-        vim.keymap.set("n", "<leader>e", function()
-            snacks.explorer({
-                finder = "explorer",
-                hidden = true,
-            })
-        end)
+		snacks.indent.enable()
 
-        vim.keymap.set("n", "<leader>q", function()
-            snacks.bufdelete()
-        end)
+		vim.keymap.set("n", "<leader>e", function()
+			snacks.explorer(config.explorer)
+		end)
 
-        vim.keymap.set("n", "<leader>ff", function()
-            snacks.picker.files({
-                hidden = true,
-            })
-        end)
-        vim.keymap.set("n", "<leader>fg", function()
-            snacks.picker.grep({
-                hidden = true,
-            })
-        end)
-        vim.keymap.set("n", "<leader>fh", function()
-            snacks.picker.recent({ hidden = true })
-        end)
-    end,
+		vim.keymap.set("n", "<leader>q", function()
+			snacks.bufdelete()
+		end)
+
+		vim.keymap.set("n", "<leader>ff", function()
+			snacks.picker.files(config.files)
+		end)
+		vim.keymap.set("n", "<leader>fg", function()
+			snacks.picker.grep(config.grep)
+		end)
+		vim.keymap.set("n", "<leader>fh", function()
+			snacks.picker.recent(config.recent)
+		end)
+		vim.keymap.set("n", "<leader>fs", function()
+			snacks.picker.lsp_workspace_symbols(config.lsp_workspace_symbols)
+		end)
+	end,
 }
