edcf1bfc61eacd00470fdeb2200275ba6c42b795
Felix Salcher <dev.felix.salcher@gmail.com>
2025-02-09 15:21:56 +0100

    chnage font-size from 12 to 10

--- nix/home/applications/ghostty.nix
+++ nix/home/applications/ghostty.nix
@@ -6,7 +6,7 @@
       gtk-titlebar = false;
 
       font-family = "JetBrainsMono Nerd Font";
-      font-size = 12;
+      font-size = 10;
 
       adjust-underline-position = 6;
       adjust-underline-thickness = 1;
