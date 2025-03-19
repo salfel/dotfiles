86912eef2011764969f5113ccf21ea5ba5f747c1
Felix Salcher <dev.felix.salcher@gmail.com>
2025-03-19 12:23:19 +0100

    add gcc-arm-embedded to global packages

--- nix/modules/hardware.nix
+++ nix/modules/hardware.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ platformio kicad easyeda2kicad ];
+  environment.systemPackages = with pkgs; [
+    platformio
+    kicad
+    easyeda2kicad
+    gcc-arm-embedded
+  ];
 
   services.udev.packages = with pkgs; [
     platformio-core
