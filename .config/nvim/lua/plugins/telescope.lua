cd565441c8ce121fb3b12b2c3647c3180d22741a
Felix Salcher <dev.felix.salcher@gmail.com>
2025-03-19 20:53:21 +0100

    replace snacks.nvim with previous implementations

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,59 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+        local actions = require("telescope.actions")
+
+        require("telescope").setup({
+            defaults = {
+                vimgrep_arguments = {
+                    "rg",
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                    "--hidden",
+                },
+                file_ignore_patterns = { "node_modules", "vendor", ".git/", "target", "dist", ".pio", "out" },
+                mappings = {
+                    i = {
+                        ["<esc>"] = actions.close,
+                    },
+                },
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    no_ignore = true,
+                },
+                oldfiles = {
+                    prompt_title = "History",
+                },
+            },
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                },
+            },
+        })
+
+        require("telescope").load_extension("fzf")
+
+        local wk = require("which-key")
+        local builtin = require("telescope.builtin")
+
+        wk.add({
+            { "<leader>ff", builtin.find_files,            desc = "Find Files" },
+            { "<leader>fh", builtin.oldfiles,              desc = "History" },
+            { "<leader>fg", builtin.live_grep,             desc = "Live Grep" },
+            { "<leader>fb", builtin.buffers,               desc = "Buffers" },
+            { "<leader>fs", builtin.lsp_workspace_symbols, desc = "Lsp Symbols" },
+        })
+    end,
+}
