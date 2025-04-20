cc56572cd7b2a6481ce5cc11e8746562842fd419
Felix Salcher <dev.felix.salcher@gmail.com>
2025-04-20 09:29:44 +0200

    update hostname to salfel

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -9,7 +9,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "salfel";
 
   # Enable networking
   networking.networkmanager.enable = true;
