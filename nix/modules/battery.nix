00e20e6cbc0c1f7680229dda6e4f3f006e1f36b6
Felix Salcher <dev.felix.salcher@gmail.com>
2025-04-20 19:13:36 +0200

    add auto-cpufreq daemon

--- nix/modules/battery.nix
+++ nix/modules/battery.nix
@@ -1,9 +1,20 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ powertop tlp auto-cpufreq ];
+  environment.systemPackages = with pkgs; [ auto-cpufreq ];
 
-  services.tlp = {
+  services.auto-cpufreq = {
     enable = true;
-    settings = { };
+    settings = {
+      battery = {
+        governor = "powersave";
+        boost = false;
+        temperature_limit = 80;
+      };
+      charger = {
+        governor = "performance";
+        boost = true;
+        temperature_limit = 90;
+      };
+    };
   };
 
   services.power-profiles-daemon.enable = false;
