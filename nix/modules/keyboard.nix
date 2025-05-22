c09b60b2bc81e2e76abd6644ecb89c6755acadbd
Felix Salcher <dev.felix.salcher@gmail.com>
2025-05-22 22:33:57 +0200

    move config to nix file

--- nix/modules/keyboard.nix
+++ nix/modules/keyboard.nix
@@ -1,5 +1,37 @@
 { pkgs, ... }:
-let kmonadCfg = builtins.readFile ./home-row-mods.kbd;
+let
+  keyboard = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+  kmonadCfg = ''
+    (defcfg
+        input (device-file "${keyboard}")
+        output (uinput-sink "Integrated Keyboard")
+        fallthrough true
+    )
+
+    (defsrc
+        capslock esc
+
+        ;; Home row modifiers
+        a    s    d    f    g    h    j    k    l    ;
+    )
+
+    (defalias
+        met_a (tap-hold-next-release 200 a lmet)
+        alt_s (tap-hold-next-release 200 s lalt)
+        sft_d (tap-hold-next-release 200 d lsft)
+        ctl_f (tap-hold-next-release 200 f lctl)
+
+        ctl_j (tap-hold-next-release 200 j rctl)
+        sft_k (tap-hold-next-release 200 k rsft)
+        alt_l (tap-hold-next-release 200 l lalt)
+        met_; (tap-hold-next-release 200 ; rmet)
+    )
+
+    (deflayer homerowmods
+        esc capslock @met_a   @alt_s   @sft_d   @ctl_f   g   h   @ctl_j   @sft_k   @alt_l   @met_;
+    )
+
+  '';
 in {
   environment.systemPackages = with pkgs; [ kmonad ];
 
@@ -7,7 +39,7 @@ in {
     enable = true;
     keyboards = {
       "integrated-keyboard" = {
-        device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+        device = keyboard;
         config = kmonadCfg;
       };
     };
