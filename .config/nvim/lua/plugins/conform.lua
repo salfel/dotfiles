6d14ffb25857516bba1a6748f6363ec786f456ae
Felix Salcher <dev.felix.salcher@gmail.com>
2025-05-26 20:37:16 +0200

    replace none-ls with conform for formatting

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			rust = { "dxfmt" },
+			go = { "gofmt" },
+			python = { "black" },
+			cpp = { "clang-format" },
+			astro = { "prettier" },
+			javascript = { "biome" },
+			typescript = { "biome" },
+			nix = { "alejandra" },
+		},
+		format_on_save = {
+			timeout_ms = 200,
+			lsp_format = "fallback",
+		},
+	},
+}
