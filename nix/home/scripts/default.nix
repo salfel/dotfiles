d10cc8df587d9dd24c586cdfd31eb860f07e6a44
Felix Salcher <dev.felix.salcher@gmail.com>
2025-06-08 23:19:01 +0200

    remove networkmanager script

--- nix/home/scripts/default.nix
+++ nix/home/scripts/default.nix
@@ -1,7 +1,4 @@
 {pkgs, ...}: let
   sessionizer-script =
     pkgs.writeScriptBin "sessionizer" (builtins.readFile ./sessionizer.sh);
-  networkmanager-script =
-    pkgs.writeScriptBin "networkmanager"
-    (builtins.readFile ./networkmanager.sh);
 in {home.packages = [sessionizer-script];}
