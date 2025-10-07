d2fbefe88bc9c22c4ff34dbf461442a0a03cc7ab
Felix Salcher <dev.felix.salcher@gmail.com>
2025-10-07 08:48:52 +0200

    update structure to enable multiple keyboards, add multi tapping

--- nix/modules/keyboard.nix
+++ nix/modules/keyboard.nix
@@ -1,46 +1,50 @@
 {pkgs, ...}: let
-  keyboard = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-  kmonadCfg = ''
-    (defcfg
-        input (device-file "${keyboard}")
-        output (uinput-sink "Integrated Keyboard")
-        fallthrough true
-    )
+  keyboards = [
+    "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+    "/dev/input/event13"
+  ];
 
-    (defsrc
-        capslock esc
+  mk-keyboard = keyboard-name: {
+    name = builtins.baseNameOf keyboard-name;
+    value = {
+      device = keyboard-name;
+      config = ''
+        (defcfg
+          input (device-file "${keyboard-name}")
+          output (uinput-sink "Integrated Keyboard")
+          fallthrough true
+        )
 
-        ;; Home row modifiers
-        a    s    d    f    g    h    j    k    l    ;
-    )
+        (defsrc
+          capslock esc
 
-    (defalias
-        alt_a (tap-hold-next-release 200 a lalt)
-        met_s (tap-hold-next-release 200 s lmet)
-        sft_d (tap-hold-next-release 200 d lsft)
-        ctl_f (tap-hold-next-release 200 f lctl)
+          ;; Home row modifiers
+          a    s    d    f    g    h    j    k    l    ;
+        )
 
-        ctl_j (tap-hold-next-release 200 j rctl)
-        sft_k (tap-hold-next-release 200 k rsft)
-        met_l (tap-hold-next-release 200 l rmet)
-        alt_; (tap-hold-next-release 200 ; lalt)
-    )
+        (defalias
+          alt_a (multi-tap 200 (tap-hold-next-release 150 a lalt) a)
+          met_s (multi-tap 200 (tap-hold-next-release 150 s lmet) s)
+          sft_d (multi-tap 200 (tap-hold-next-release 150 d lsft) d)
+          ctl_f (multi-tap 200 (tap-hold-next-release 150 f lctl) f)
 
-    (deflayer homerowmods
-        esc capslock @alt_a   @met_s   @sft_d   @ctl_f   g   h   @ctl_j   @sft_k   @met_l   @alt_;
-    )
+          ctl_j (multi-tap 200 (tap-hold-next-release 150 j rctl) j)
+          sft_k (multi-tap 200 (tap-hold-next-release 150 k rsft) k)
+          met_l (multi-tap 200 (tap-hold-next-release 150 l rmet) l)
+          alt_; (multi-tap 200 (tap-hold-next-release 150 ; lalt) ;)
+        )
 
-  '';
+        (deflayer homerowmods
+          esc capslock @alt_a @met_s @sft_d @ctl_f g h @ctl_j @sft_k @met_l @alt_;
+        )
+      '';
+    };
+  };
 in {
   environment.systemPackages = with pkgs; [kmonad];
 
   services.kmonad = {
     enable = true;
-    keyboards = {
-      "integrated-keyboard" = {
-        device = keyboard;
-        config = kmonadCfg;
-      };
-    };
+    keyboards = builtins.listToAttrs (map mk-keyboard keyboards);
   };
 }
