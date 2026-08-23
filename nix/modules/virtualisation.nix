02dace35e4b0c2df64a2f7c6f6ebaae9a328b78c
Felix Salcher <dev.felix.salcher@gmail.com>
2026-08-23 19:23:18 +0200

    added virt-manager

--- nix/modules/virtualisation.nix
+++ nix/modules/virtualisation.nix
@@ -1,7 +1,20 @@
-{
+{pkgs, ...}: {
   virtualisation = {
     docker.enable = true;
+
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = false;
+        swtpm.enable = true;
+      };
+    };
+
+    spiceUSBRedirection.enable = true;
   };
 
-  users.users.felix.extraGroups = ["kvm" "docker"];
+  programs.virt-manager.enable = true;
+
+  users.users.felix.extraGroups = ["kvm" "docker" "libvirtd"];
 }
